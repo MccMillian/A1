250669e145e1a19e63b1dd59b0a3bbcddcc67d8a
Sebastian Nozoe <sebastian.nozoe@gmail.com>
2025-06-10 17:38:15 -0500

    Leer publicaciones desde archivo JSON

--- index.php
+++ index.php
@@ -1,38 +1,21 @@
 <?php
-// Esta linea indica que inicia el código PHP
-// ##########################################
-// Se crea un array llamado $posts que contiene varios arrays 
+// Leer el contenido del archivo JSON
+$json = file_get_contents("posts.json"); // Lee el archivo como un string
 
-$posts = [
-    [
-        "id" => 1,
-        "titulo" => "Bienvenido a mi blog",
-        "Autor" => "Sebastian",
-        "fecha" => "2023-10-01",
-        "contenido" => "Este es el primer post de mi blog"
-    ],
-// Cada array representa una publicación
-    [
-        "id" => 2,
-        "titulo" => "Segundo articulo",
-        "Autor" => "Sebastian",
-        "fecha" => "2023-10-02",
-        "contenido" => "Usando arrays en PHP"
-    ]
-];
-//Aqui se incluye el encabezado
-// Este bloque inicia un bucle para recorrer cada post dentro del array $posts
+// Convertir el JSON a un array de PHP
+$posts = json_decode($json, true); // true = convierte en array asociativo
+
+
 include("includes/header.php");
 ?>
 <h2> ultimas publicaciones </h2>
 <?php foreach ($posts as $post): ?>
     <div style= "border:1px solid #ccc; padding: 10px; margin-bottom: 10px;">
      <h3><?php echo $post["titulo"]; ?></h3>
-        <small><strong><?php echo $post["Autor"]; ?></strong> - <?php echo $post["fecha"]; ?></small>
+        <small><strong><?php echo $post["autor"]; ?></strong> - <?php echo $post["fecha"]; ?></small>
         <p><?php echo $post["contenido"]; ?></p>
     </div>
 <?php endforeach; ?>
 <?php
-// Incluir pie de página
 include("includes/footer.php");
 ?>
